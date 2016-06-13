b60e3d8c2ea90a8ebbd42f1621ea0a41679f2390
zzl <zhengzl0715@163.com>
2016-06-13 21:50:25 +0800

    Add handling logic

--- src/main/java/cc/gauto/MainServer.java
+++ src/main/java/cc/gauto/MainServer.java
@@ -1,5 +1,6 @@
 package cc.gauto;
 
+import cc.gauto.tcpserver.BasestationServer;
 import org.apache.log4j.Logger;
 
 /**
@@ -8,9 +9,11 @@ import org.apache.log4j.Logger;
 public class MainServer {
     private static final Logger logger = Logger.getLogger(MainServer.class);
 
-    public static void main(String []args) {
-        System.out.println("hello");
-        logger.info("info");
-        logger.error("error");
+    public static void main(String []args) throws Exception {
+        int tcpPort = 8081;
+
+        BasestationServer server = new BasestationServer(tcpPort);
+        server.start();
+        server.join();
     }
 }
---
+++ src/main/java/cc/gauto/tcpserver/BasestationHandler.java
@@ -0,0 +1,64 @@
+package cc.gauto.tcpserver;
+
+/**
+ * @Author Zhilong Zheng
+ * @Email zhengzl0715@163.com
+ * @Date 2016-06-13 20:47
+ */
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import org.apache.log4j.Logger;
+import org.apache.log4j.net.SyslogAppender;
+
+public class BasestationHandler extends SimpleChannelInboundHandler {
+    private static final Logger logger = Logger.getLogger(BasestationHandler.class.getName());
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        String body = (String)msg;
+        System.out.println(body);
+    }
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext channelHandlerContext, Object o) throws Exception {
+    }
+
+    /*
+         * 新的client链接
+         */
+    @Override
+    public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
+        logger.info("A new client connected: " + ctx.channel().remoteAddress());
+        super.channelRegistered(ctx);
+    }
+
+    /*
+     * client断开链接
+     */
+    @Override
+    public void channelUnregistered(ChannelHandlerContext ctx) throws Exception {
+        logger.info("A client disconnected: " + ctx.channel().localAddress());
+        super.channelUnregistered(ctx);
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        super.channelActive(ctx);
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        super.channelInactive(ctx);
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        ctx.flush();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        logger.warn("Unexpected exception from channel. " + cause.getMessage());
+        ctx.close();
+    }
+}
